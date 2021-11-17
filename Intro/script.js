6712c5ac33a522a590c686700ea1e49aeef0970f
Alaina Rosalez <alainarosalez@gmail.com>
2021-11-17 13:24:53 -0500

    Test Again

---
+++ Intro/script.js
@@ -0,0 +1,19 @@
+alert("Hello ninja!") //todo ctrl + / comments all highlighted text for you and can turn it off at will//
+
+//Assigning variables below and output to console.
+// var x = 5;
+// var y = 37;
+
+// var answer = x + y;
+
+// console.log(answer); //todo make sure you are looking at logs and XHR while inspecting and not Requests//
+
+//Take user input via a propmpt and then output the info via an alert.
+var name = prompt(" please enter your name")
+alert("Hello " + name + ". How are you today?")
+
+//? On the test//
+//! Camelcase = /f/irst /N/ame
+//! PascalCase = /F/irst /N/ame
+//! Snakecase = first_name
+//? On the test//
